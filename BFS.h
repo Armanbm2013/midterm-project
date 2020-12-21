5eb06ca1ffe8101fddb918e024b62bfb9646d8fe
Armanbm2013 <armanbeykmohammadi@gmail.com>
2020-12-21 18:50:47 +0330

    creating another .h file for new algorithm

--- BFS.h
+++ BFS.h
@@ -9,5 +9,5 @@ void BFS(const std::vector<size_t> &init, const std::vector<size_t> &goal);
 bool is_goal(const std::vector<size_t> &state, const std::vector<size_t> &goal);
 void solved(size_t steps, const std::vector<size_t> &goal);
 void print_puzzle(size_t steps, const std::vector<size_t> &state);
-void another_algorithm_BFS(const std::vector<size_t> &init, const std::vector<size_t> &goal);
+
 #endif
---
+++ another_way_BFS.h
@@ -0,0 +1,10 @@
+#ifndef awbfs_h
+#define awbfs_h
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+void another_algorithm_BFS(const std::vector<size_t> &init, const std::vector<size_t> &goal);
+
+#endif
