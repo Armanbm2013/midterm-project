75316c5f0b8878f14edb8055ea6a6d33960ebe3b
Armanbm2013 <armanbeykmohammadi@gmail.com>
2020-12-22 12:50:20 +0330

    it works perfectly!

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ int main()
     std::vector<size_t> a{1, 2, 3, 4, 8, 0, 7, 6, 5};
     std::vector<size_t> b{1, 2, 3, 6, 0, 4, 8, 5, 7};
     std::vector<size_t> c{1, 5, 7, 6, 8, 4, 3, 0, 2};
+    std::vector<size_t> d{3, 5, 6, 8, 1, 4, 0, 7, 2};
     std::vector<size_t> non_solvable{8, 1, 2, 0, 4, 3, 7, 6, 5};
-    another_algorithm_BFS(b, goal);
+    another_algorithm_BFS(d, goal);
     return 0;
 }
