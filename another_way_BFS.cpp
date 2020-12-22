51165beaa00c60b7087180066ce38f3c73f3f96a
Armanbm2013 <armanbeykmohammadi@gmail.com>
2020-12-22 11:27:01 +0330

    still trying to debug, the while loop goes infinitly

--- another_way_BFS.cpp
+++ another_way_BFS.cpp
@@ -48,8 +48,9 @@ void another_algorithm_BFS(const std::vector<size_t> &init, const std::vector<si
                                     for (size_t j{}; j < 9; j++)
                                     {
                                         std::cout << j << "\n";
-                                        if (temp1[j] == 0)
-                                            i = j;
+                                        std::cout << i << "\n";
+                                        // if (temp1[j] == 0)
+                                        //     i = j;
                                         std::cout << "checked4\n";
                                     }
                                     std::cout << "checked3\n";
