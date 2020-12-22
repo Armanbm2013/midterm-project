e8c22b2924294d156d74708c2c9dcd5bf246c8e1
Armanbm2013 <armanbeykmohammadi@gmail.com>
2020-12-22 12:37:09 +0330

    function works correctly for step 4, let's try step 3

--- main.cpp
+++ main.cpp
@@ -4,11 +4,9 @@
 int main()
 {
     std::vector<size_t> goal{1, 2, 3, 4, 5, 6, 7, 8, 0};
-    std::vector<size_t> a{0, 4, 3, 8, 2, 5, 1, 6, 7};
-    std::vector<size_t> b{0, 7, 5, 4, 2, 1, 8, 6, 3};
-    std::vector<size_t> c{3, 2, 0, 7, 5, 6, 4, 1, 8};
-    std::vector<size_t> d{1, 2, 3, 4, 8, 0, 7, 6, 5};
+    std::vector<size_t> a{1, 2, 3, 4, 8, 0, 7, 6, 5};
+    std::vector<size_t> b{1, 2, 3, 6, 0, 4, 8, 5, 7};
     std::vector<size_t> non_solvable{8, 1, 2, 0, 4, 3, 7, 6, 5};
-    another_algorithm_BFS(d, goal);
+    another_algorithm_BFS(b, goal);
     return 0;
 }
