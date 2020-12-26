e9bc03cd1845fcf0343e06f04741ec092f1aca2a
Armanbm2013 <armanbeykmohammadi@gmail.com>
2020-12-26 17:03:27 +0330

    process_DFS parameters changed

--- DFS.h
+++ DFS.h
@@ -5,7 +5,7 @@
 #include <vector>
 #include <stack>
 
-void Process_DFS(const std::vector<size_t> &init, const std::vector<size_t> &goal, std::vector<std::vector<size_t>> &dfs, std::stack<std::vector<size_t>> &stack);
+void Process_DFS(std::vector<std::vector<size_t>> &dfs, std::stack<std::vector<size_t>> &stack, size_t &steps);
 void DFS(const std::vector<size_t> &init, const std::vector<size_t> &goal);
 
 #endif
