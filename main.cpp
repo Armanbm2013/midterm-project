efa4652b5b27745a935b9c1db0f8bce8e4e92480
Armanbm2013 <armanbeykmohammadi@gmail.com>
2020-12-30 18:12:36 +0330

    ready for upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <chrono>
 #include "BFS.h"
 #include "another_way_BFS.h"
 #include "apmidproject.h"
@@ -6,23 +5,13 @@
 
 int main()
 {
-    auto start = std::chrono::high_resolution_clock::now(); //run time calculator
-
+    hello_game(); //welcome message
     do
     {
-        hello_game(); //welcome message
-
         std::vector<size_t> first_state{own_8_puzzle_vs_random()}; //determinig first state
-
-        std::vector<size_t> goal{input_goal_vs_default_goal()}; //determining goal state
-
+        std::vector<size_t> goal{input_goal_vs_default_goal()};    //determining goal state
         choose_algorithm(first_state, goal);
-
     } while (continue_game());
-
-    auto stop = std::chrono::high_resolution_clock::now();                                                      //run time calculator
-    auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);                             //run time calculator
-    std::cout << "******Time Taken To Solve The Puzzle: " << duration.count() << " Seconds******" << std::endl; //run time calculator
 
     //default goal: std::vector<size_t> goal{1, 2, 3, 4, 5, 6, 7, 8, 0};
     //tests:
