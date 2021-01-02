68ff81216c0c0713337bf0cb7a6cd1a8b97d4553
Armanbm2013 <armanbeykmohammadi@gmail.com>
2021-01-02 16:26:29 +0330

    Node created

--- Node.cpp
+++ Node.cpp
@@ -1,8 +1,14 @@
 #include "Node.h"
-Node::Node()
+
+Node::Node(std::vector<int> &state)
 {
+    this->state = move(state);
+    parents.push_back(this->state);
 }
 
-Node::~Node()
+Node::Node(const Node &parent, std::vector<int> &state)
 {
+    parents = parent.parents;
+    this->state = move(state);
+    parents.push_back(this->state);
 }
--- Node.h
+++ Node.h
@@ -1,11 +1,17 @@
 #ifndef Node_H
 #define Node_H
 
+#include <iostream>
+#include <vector>
+
 class Node
 {
 public:
-    Node();
-    ~Node();
+    Node(std::vector<int> &state);
+    Node(const Node &parent, std::vector<int> &state);
+
+    std::vector<int> state{};
+    std::vector<std::vector<int>> parents{};
 };
 
 #endif
